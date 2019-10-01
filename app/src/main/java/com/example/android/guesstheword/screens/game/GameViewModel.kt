2ffcbbb5957a7c63a280b89eb0802626cf4a3b2e
supanat2 <59161123@go.buu.ac.th>
2019-10-01 22:47:27 +0700

    Populate

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,10 +1,91 @@
 package com.example.android.guesstheword.screens.game
 
-import android.util.Log
+
 import androidx.lifecycle.ViewModel
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.android.guesstheword.R
+import com.example.android.guesstheword.databinding.GameFragmentBinding
 
 class GameViewModel : ViewModel() {
+    // The current word
+    var word = ""
+    // The current score
+    var score = 0
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
     init {
+        resetList()
+        nextWord()
         Log.i("GameViewModel", "GameViewModel created!")
     }
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        if (!wordList.isEmpty()) {
+            //Select and remove a word from the list
+            word = wordList.removeAt(0)
+        }
+
+
+    }
+    /** Methods for buttons presses **/
+    fun onSkip() {
+        if (!wordList.isEmpty()) {
+            score--
+        }
+        nextWord()
+    }
+
+    fun onCorrect() {
+        if (!wordList.isEmpty()) {
+            score++
+        }
+        nextWord()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("GameViewModel", "GameViewModel destroyed!")
+    }
+
+
 }
